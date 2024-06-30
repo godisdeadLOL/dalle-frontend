9237fc6f89ae955a9641270e373e5122673af9c7
YourUsername <you@example.com>
2024-06-30 12:29:21 +0300

    Using fontsource instead of loading from cdm

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { render } from 'preact'
 import { App } from './app.jsx'
+
 import './styles/tailwind.css'
+import '@fontsource-variable/inter';
 
 render(<App />, document.getElementById('app'))
